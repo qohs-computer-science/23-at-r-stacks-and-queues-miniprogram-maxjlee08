58fce8f4aad5e4a23b419d1f47a9dd600ca6772d
maxjlee08 <maxjlee08@gmail.com>
2025-12-19 20:44:41 +0000

    Done

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -5,6 +5,53 @@ import java.util.LinkedList;
 
 class StackQueueMini {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Stack<Integer> stack = new Stack<>();
+    Queue<Integer> queue = new LinkedList<>();
 
-  } // end main
-} // end class
+    for(int i = 0; i < 5; i++) {
+      System.out.print("Enter a number: ");
+      stack.push(input.nextInt());
+    }
+    
+    System.out.println("Original Stack Bottom "+ stack + " Top");
+
+    Stack<Integer> tempStack = new Stack<>();
+
+    while(!stack.isEmpty()) {
+      tempStack.push(stack.pop());
+    } 
+
+    while(!tempStack.isEmpty()) {
+      int value = tempStack.pop();
+      stack.push(value);
+      stack.push(value);
+    }
+
+    System.out.println("Modified Stack Bottom "+ stack + " Top");
+
+    for(int i = 0; i<10; i++){
+      System.out.print("Enter a number: ");
+      queue.add(input.nextInt());
+    }
+    input.close();
+
+    System.out.println("Original Queue Front " + queue + " back");
+
+    Queue<Integer> tempQueue = new LinkedList<>();
+    int size = queue.size();
+    for(int i = 0; i < size; i++) {
+      if(queue.peek() % 2 == 0) {
+        tempQueue.add(queue.remove());
+      } else {
+        queue.add(queue.remove());
+      }
+
+      while(!tempQueue.isEmpty()) {
+        queue.add(tempQueue.remove());
+      }
+    }
+      System.out.println("Modified Queue Front " + queue + " back");
+  }
+
+}
